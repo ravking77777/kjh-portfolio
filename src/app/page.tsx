159f2bbda54b47690ce474a370fdb4a0543280e4
JaeHong KIM <144203722+ravking77777@users.noreply.github.com>
2024-06-20 12:09:41 +0900

    오류 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,19 +8,16 @@ export default function Home() {
     <>
     <header className="relative w-full bg-gray-900 py-12 md:py-20 lg:py-24">
       <div className="absolute inset-0">
-        <img
-          src="/NewGR200.png"
-          alt="Background"
-          className="w-full h-full object-cover opacity-30"
-          style={{
-            objectFit: "cover",
-            maxHeight: "100%"
-          }}
-        />
+      <Image
+      alt="Background"
+      src="/NewGR200.png"
+      layout="fill"
+      className="object-cover opacity-30"
+      />
       </div>
       <div className="relative container mx-auto px-4 md:px-6">
         <div className="mx-auto max-w-3xl text-center">
-          <h1 className="text-4xl  tracking-tight text-gray-50 sm:text-5xl lg:text-6xl">김재홍(JaeHong Kim)</h1>
+          <h1 className="text-4xl font-bold tracking-tight text-gray-50 sm:text-5xl lg:text-6xl">김재홍(JaeHong Kim)</h1>
           <p className="mt-4 text-lg text-gray-400">
             게임 개발 및 기획자 | 인디게임을 좋아하고 1인개발을 지향
           </p>
