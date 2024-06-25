792fb586eb718668313bf475688427ee20ea805a
JaeHong KIM <144203722+ravking77777@users.noreply.github.com>
2024-06-25 10:13:39 +0900

    submit 버튼 교체

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,25 +5,14 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-gray-950 dark:focus-visible:ring-gray-300",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md",
   {
     variants: {
       variant: {
-        default: "bg-gray-50 text-gray-900 hover:bg-gray-50/90",
-        destructive:
-          "bg-red-900 text-gray-50 hover:bg-red-900/90",
-        outline:
-          "border-gray-800 bg-gray-950 hover:bg-gray-800 hover:text-gray-50",
-        secondary:
-          "bg-gray-800 text-gray-50 hover:bg-gray-800/80",
-        ghost: "hover:bg-gray-800 hover:text-gray-50",
-        link: "underline-offset-4 hover:underline text-gray-50",
+        default: "bg-black",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        default: "h-10 px-4 py-12",
       },
     },
     defaultVariants: {
